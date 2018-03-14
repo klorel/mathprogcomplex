20e13e83372d9ca18735af2344399ffce818f347
Gilles Bareilles <gilles.bareilles@gmail.com>
2018-03-14 17:34:14 +0100

    More explicit comments

--- src_SOShierarchy/func_definitions.jl
+++ src_SOShierarchy/func_definitions.jl
@@ -5,16 +5,18 @@
 """
 function get_ki(problem::Problem)
     println("\n=== get_ki(problem::Problem)")
+    println("Compute the degree of each constraint of `pb`.")
     return Dict{String, Int}()
 end
 
 """
     check_di_ki!(d_i, k_i)
 
-    Enforce the condition .. on each constraint (if needed).
+    Enforce the condition d_i - k_i ≥ 0 on each constraint (if needed).
 """
 function check_di_ki!(d_i, k_i)
     println("\n=== check_di_ki!(d_i, k_i)")
+    println("Enforce the condition d_i - k_i ≥ 0 on each constraint (if needed).")
 end
 
 
@@ -33,6 +35,7 @@ type SparsityPattern end
 """
 function compute_sparsitypattern(problem::Problem, d_i, k_i)
     println("\n=== compute_sparsitypattern(problem::Problem, d_i, k_i)")
+    println("Compute the sparsity_pattern corresponding to the given partial orders.")
     sparsity_pattern = SparsityPattern()
     return sparsity_pattern::SparsityPattern
 end
@@ -44,16 +47,17 @@ end
 """
 function compute_chordalextension!(sparsity_pattern::SparsityPattern)
     println("\n=== compute_chordalextension!(sparsity_pattern::SparsityPattern)")
+    println("Compute the chordal extension on the provided sparsity_pattern.")
 end
 
 """
     maxcliques = compute_maxcliques(sparsity_pattern)
 
-    Compute an `Array{Set{Variable}}` describing the maximum cliques on the
-    provided sparsity_pattern.
+    Compute a `Array{Set{Variable}}` describing the maximum cliques on the provided sparsity_pattern.
 """
 function compute_maxcliques(sparsity_pattern::SparsityPattern)
     println("\n=== compute_maxcliques(sparsity_pattern::SparsityPattern)")
+    println("Compute a `Array{Set{Variable}}` describing the maximum cliques on the provided sparsity_pattern.")
     maxcliques = Array{Set{Variable}}()
     return maxcliques::Array{Set{Variable}}
 end
@@ -62,11 +66,11 @@ end
 """
     varsbycstr = compute_varsbycstr(problem)
 
-    Compute a `Dict{String, Set{Variable}}` providing the set of variables
-    involved in a constraint.
+    Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in each constraint.
 """
 function compute_varsbycstr(problem::Problem)
     println("\n=== compute_varsbycstr(problem::Problem)")
+    println("Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in each constraint.")
     varsbycstr = Dict{String, Set{Variable}}()
     return varsbycstr::Dict{String, Set{Variable}}
 end
@@ -74,11 +78,11 @@ end
 """
     cliquevarsbycstr = compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)
 
-    Compute a `Dict{String, Set{Variable}}` providing the set of variables
-    involved in the SDP localizing matrix corresponding to a constraint.
+    Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in the SDP localizing matrix corresponding to each constraint.
 """
 function compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)
     println("\n=== compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)")
+    println("Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in the SDP localizing matrix corresponding to each constraint.")
     cliquevarsbycstr = Dict{String, Set{Variable}}()
     return cliquevarsbycstr::Dict{String, Set{Variable}}
 end
@@ -86,11 +90,11 @@ end
 """
     orderbyclique = compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)
 
-    Compute a `Dict{Int, Int}` providing the relaxation order corresponding to
-    each clique.
+    Compute a `Array{Int}` providing the relaxation order corresponding to each clique.
 """
 function compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)
     println("\n=== compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)")
+    println("Compute a `Array{Int}` providing the relaxation order corresponding to each clique.")
     orderbyclique = Dict{Int, Int}()
     return orderbyclique::Dict{Int, Int}
 end
@@ -101,11 +105,11 @@ end
 """
     B_i_dict = compute_Bibycstr(problem, max_cliques, cliquevarsbycstr, orderbyclique)
 
-    Compute a the decomposition of each SDP constraint on the moment basis,
-    yielding several matrices B_i,α,β for each constraint i.
+    Compute a the decomposition of each SDP constraint on the moment basis, yielding several matrices B_i,α,β for each constraint i.
 """
 function compute_Bibycstr(problem, max_cliques, cliquevarsbycstr, orderbyclique)
     println("\n=== compute_Bibycstr(problem, max_cliques, cliquevarsbycstr, orderbyclique)")
+    println("Compute a the decomposition of each SDP constraint on the moment basis, yielding several matrices B_i,α,β for each constraint i.")
     return
 end
 
@@ -113,11 +117,11 @@ end
 """
     SDP_SOS = build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
 
-    Build the primal SDP corresponding to the dual SOS hierarchy for the provided
-    problem.
+    Build the primal SDP corresponding to the dual SOS hierarchy for the provided problem.
 """
 function build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
     println("\n=== build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)")
+    println("Build the primal SDP corresponding to the dual SOS hierarchy for the provided problem.")
     return
 end
 
@@ -129,5 +133,6 @@ end
 """
 function make_JuMPproblem(SDP_SOS)
     println("\n=== make_JuMPproblem(SDP_SOS)")
+    println("Convert the SDP_SOS problem into a JuMP problem")
     return
 end
