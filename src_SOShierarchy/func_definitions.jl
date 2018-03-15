5be4db020b5e00d635a05389a9387b2869aa28a4
Gilles Bareilles <gilles.bareilles@gmail.com>
2018-03-15 11:47:16 +0100

    evolve skeleton

--- src_SOShierarchy/func_definitions.jl
+++ src_SOShierarchy/func_definitions.jl
@@ -1,26 +1,73 @@
 """
-    k_i = get_k_i(pb::Problem)
+    relax_ctx = set_relaxation(problem; issparse = false, ismultiordered = false, d = 1)
 
-    Compute the degree of each constraint of `pb`.
+    Build a `relax_ctx` object containing relaxation choices and problem features : order by constraint, relaxation order by constraint.
 """
-function get_ki(problem::Problem)
-    println("\n=== get_ki(problem::Problem)")
-    println("Compute the degree of each constraint of `pb`.")
-    println("-> Nb of variables:            xx")
-    println("-> Nb of poly constraints:     xx")
-    return Dict{String, Int}()
+function set_relaxation(problem; issparse = false, ismultiordered = false, d = 1)
+    println("\n=== set_relaxation(problem, issparse = false, ismultiordered = false, d = 1)")
+    println("Build a `relax_ctx` object containing relaxation choices and problem features : polynomial degree by constraint, relaxation order by constraint...")
+
+    println("-> Nb of variables:                        xx")
+    println("-> Nb of constraints:                      xx")
+    println("-> Nb of monomials:                        xx")
+    println("-> Max total degree by constraint:         xx / xx (mean/std)")
+    println("-> Number of constraints s.t. di > ki:     xx")
+    println("-> Max total degree on such constraints:   xx / xx (mean/std)")
+    println("All variables appearing in such constraints will be linked in the sparsity pattern, which will largely densify it.")
+    return
 end
 
-"""
-    check_di_ki!(d_i, k_i)
-
-    Enforce the condition d_i - k_i ≥ 0 on each constraint (if needed).
-"""
-function check_di_ki!(d_i, k_i)
-    println("\n=== check_di_ki!(d_i, k_i)")
-    println("Enforce the condition d_i - k_i ≥ 0 on each constraint (if needed).")
-    println("-> number of d_i > k_i:        xx / xx constraints")
-end
+# """
+#     di = set_uniformdi(problem, d)
+#
+#     Return a `Dict{String, Int}` corresponding to the relaxation degree of each constraint, uniformly initialized to `d` over constraints.
+# """
+# function set_uniformdi(problem::Problem, d::Int)
+#     println("\n=== set_uniformdi(problem::Problem, d::Int)")
+#     println("Return a `Dict{String, Int}` corresponding to the relaxation degree of each constraint, uniformly initialized to `d` over constraints.")
+#     return Dict{String, Int}([(cstrname, d) for cstrname in keys(problem.constraints)])
+# end
+#
+#
+# """
+#     ki = get_ki(pb::Problem)
+#
+#     Compute the degree of each constraint of `pb`.
+# """
+# function get_ki(problem::Problem)
+#     println("\n=== get_ki(problem::Problem)")
+#     println("Compute the degree of each constraint of `pb`.")
+#
+#     ki = Dict{String, Int}()
+#     for (cstrname, cstr) in problem.constraints
+#         deg = cstr.p.degree
+#         ki[cstrname] = max(deg.explvar, deg.conjvar)
+#     end
+#
+#     println("-> Nb of variables:            $(length(problem.variables))")
+#     println("-> Nb of poly constraints:     $(length(problem.constraints))")
+#     return ki
+# end
+#
+# """
+#     check_di_ki!(di, ki)
+#
+#     Enforce the condition di - ki ≥ 0 on each constraint (if needed).
+# """
+# function check_di_ki!(di, ki)
+#     println("\n=== check_di_ki!(di, ki)")
+#     println("Enforce the condition di - ki ≥ 0 on each constraint (if needed).")
+#
+#     excessorders = 0
+#     for (cstrname, d_cstr) in di
+#         if (d_cstr - ki[cstrname] < 0)
+#             d_cstr = ki[cstrname]
+#         end
+#         (d_cstr > ki[cstrname]) && excessorders += 1
+#     end
+#
+#     println("-> number of di > ki:        xx / xx constraints")
+# end
 
 
 
@@ -32,15 +79,15 @@ end
 type SparsityPattern end
 
 """
-    sparsity_pattern = compute_sparsitypattern(problem, d_i, k_i)
+    sparsity_pattern = compute_sparsitypattern(problem, di, ki)
 
     Compute the sparsity_pattern corresponding to the given partial orders.
 """
-function compute_sparsitypattern(problem::Problem, d_i, k_i)
-    println("\n=== compute_sparsitypattern(problem::Problem, d_i, k_i)")
+function compute_sparsitypattern(problem::Problem, relax_ctx)
+    println("\n=== compute_sparsitypattern(problem::Problem, relax_ctx)")
     println("Compute the sparsity_pattern corresponding to the given partial orders.")
-    println("-> Nb vertex / edges:              xx / xx")
-    println("-> Order by vertex mean / std:     xx / xx")
+    println("-> Nb vertex / edges:      xx / xx")
+    println("-> Order by vertex:        xx / xx (mean/std)")
     sparsity_pattern = SparsityPattern()
     return sparsity_pattern::SparsityPattern
 end
@@ -53,7 +100,7 @@ end
 function compute_chordalextension!(sparsity_pattern::SparsityPattern)
     println("\n=== compute_chordalextension!(sparsity_pattern::SparsityPattern)")
     println("Compute the chordal extension on the provided sparsity_pattern.")
-    println("-> Nb added edges: xx")
+    println("-> Nb added edges:                 xx")
     println("-> Nb vertex / edges:              xx / xx")
     println("-> Order by vertex:                xx / xx (mean/std)")
 end
@@ -67,7 +114,7 @@ function compute_maxcliques(sparsity_pattern::SparsityPattern)
     println("\n=== compute_maxcliques(sparsity_pattern::SparsityPattern)")
     println("Compute a `Array{Set{Variable}}` describing the maximum cliques on the provided sparsity_pattern.")
     println("-> Nb cliques / nb vertex:         xx / xx")
-    println("-> Nb variables by clique:         xx / xx (mean/std)")
+    println("-> Nb vertex by clique:            xx / xx (mean/std)")
     maxcliques = Array{Set{Variable}}()
     return maxcliques::Array{Set{Variable}}
 end
@@ -82,7 +129,7 @@ function compute_varsbycstr(problem::Problem)
     println("\n=== compute_varsbycstr(problem::Problem)")
     println("Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in each constraint.")
     println("-> Nb poly constraints:                xx")
-    println("-> Nb variables by poly constraints:   xx / xx (mean/std)")
+    println("-> Nb variables by poly constraint:    xx / xx (mean/std)")
     varsbycstr = Dict{String, Set{Variable}}()
     return varsbycstr::Dict{String, Set{Variable}}
 end
@@ -96,18 +143,18 @@ function compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)
     println("\n=== compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)")
     println("Compute a `Dict{String, Set{Variable}}` providing the set of variables involved in the SDP localizing matrix corresponding to each constraint.")
     println("-> Nb SDP constraints:                 xx")
-    println("-> Nb variables by SDP constraints:    xx / xx (mean/std)")
+    println("-> Nb variables by SDP constraint:     xx / xx (mean/std)")
     cliquevarsbycstr = Dict{String, Set{Variable}}()
     return cliquevarsbycstr::Dict{String, Set{Variable}}
 end
 
 """
-    orderbyclique = compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)
+    orderbyclique = compute_cliqueorders(sparsity_pattern, di, varsbycstr, max_cliques)
 
     Compute a `Array{Int}` providing the relaxation order corresponding to each clique.
 """
-function compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)
-    println("\n=== compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)")
+function compute_cliqueorders(sparsity_pattern, varsbycstr, max_cliques, relax_ctx)
+    println("\n=== compute_cliqueorders(sparsity_pattern, varsbycstr, max_cliques, relax_ctx)")
     println("Compute a `Array{Int}` providing the relaxation order corresponding to each clique.")
     println("-> Nb cliques:                         xx")
     println("-> Relaxation order by clique:         xx / xx (mean/std)")
@@ -134,12 +181,12 @@ end
 
 
 """
-    SDP_SOS = build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
+    SDP_SOS = build_SDP_SOS(problem, di, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
 
     Build the primal SDP corresponding to the dual SOS hierarchy for the provided problem.
 """
-function build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
-    println("\n=== build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)")
+function build_SDP_SOS(problem, max_cliques, B_i, cliquevarsbycstr, orderbyclique, relax_ctx)
+    println("\n=== build_SDP_SOS(problem, max_cliques, B_i, cliquevarsbycstr, orderbyclique, relax_ctx)")
     println("Build the primal SDP corresponding to the dual SOS hierarchy for the provided problem.")
     println("-> Nb of SDP variables:                                xx")
     println("-> Size of the SDP variables:                          xx/xx (mean/std)")
--- src_SOShierarchy/main.jl
+++ src_SOShierarchy/main.jl
@@ -2,45 +2,43 @@ ROOT = pwd()
 include(joinpath(ROOT, "src_PowSysMod", "PowSysMod_body.jl"))
 include(joinpath("func_definitions.jl"))
 
-function main()
+# function main()
 
-    ########################################
-    # Construction du problème type
-    OPFpbs = load_OPFproblems(MatpowerInput, joinpath("data_Matpower", "matpower", "WB2.m"))
-    problem = build_globalpb!(OPFpbs)
+########################################
+# Construction du problème type
+OPFpbs = load_OPFproblems(MatpowerInput, joinpath("data_Matpower", "matpower", "WB2.m"))
+problem = build_globalpb!(OPFpbs)
 
-    println("WB2 problem built")
-    # print(problem)
+println("WB2 problem built")
+# print(problem)
 
-    ########################################
-    # Setting relaxation order by constraint
-    d_i = Dict("cstr1"=>1, "cstr2"=>1)
-    k_i = get_ki(problem)
-    check_di_ki!(d_i, k_i)
+########################################
+# Setting relaxation order by constraint
+relax_ctx = set_relaxation(problem, issparse = false, ismultiordered = false, d = 1)
 
-    ########################################
-    # Construction du sparsity pattern
-    sparsity_pattern = compute_sparsitypattern(problem, d_i, k_i)
+########################################
+# Construction du sparsity pattern
+sparsity_pattern = compute_sparsitypattern(problem, relax_ctx)
 
-    # Extension chordale et détection des cliques maximales
-    compute_chordalextension!(sparsity_pattern)
-    max_cliques = compute_maxcliques(sparsity_pattern)
+# Extension chordale et détection des cliques maximales
+compute_chordalextension!(sparsity_pattern)
+max_cliques = compute_maxcliques(sparsity_pattern)
 
-    ########################################
-    # Relaxation degree par clique and variables par constrainte
-    varsbycstr = compute_varsbycstr(problem)
-    cliquevarsbycstr = compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)
-    orderbyclique = compute_cliqueorders(sparsity_pattern, d_i, varsbycstr, max_cliques)
+########################################
+# Relaxation degree par clique and variables par constrainte
+varsbycstr = compute_varsbycstr(problem)
+cliquevarsbycstr = compute_varsbycstr(sparsity_pattern, max_cliques, varsbycstr)
+orderbyclique = compute_cliqueorders(sparsity_pattern, varsbycstr, max_cliques, relax_ctx)
 
-    ########################################
-    # Calcul des matrices B_i et pose du probleme
-    B_i = compute_Bibycstr(problem, max_cliques, cliquevarsbycstr, orderbyclique)
-    SDP_SOS = build_SDP_SOS(problem, d_i, max_cliques, B_i, cliquevarsbycstr, orderbyclique)
+########################################
+# Calcul des matrices B_i et pose du probleme
+B_i = compute_Bibycstr(problem, max_cliques, cliquevarsbycstr, orderbyclique)
+SDP_SOS = build_SDP_SOS(problem, max_cliques, B_i, cliquevarsbycstr, orderbyclique, relax_ctx)
 
-    ########################################
-    # Calcul d'une solution par un solveur
-    m = make_JuMPproblem(SDP_SOS)
+########################################
+# Calcul d'une solution par un solveur
+m = make_JuMPproblem(SDP_SOS)
 
-end
+# end
 
-main()
+# main()
