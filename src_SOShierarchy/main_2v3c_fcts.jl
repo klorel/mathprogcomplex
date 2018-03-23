980596139df6242746a059de2d7d6bd1a2aeeec0
Gilles Bareilles <gilles.bareilles@ensta-paristech.fr>
2018-03-23 14:04:17 +0100

    add Molzahn example

--- src_SOShierarchy/main_2v3c_fcts.jl
+++ src_SOShierarchy/main_2v3c_fcts.jl
@@ -6,12 +6,15 @@ include(joinpath(ROOT, "src_SOShierarchy", "func_definitions.jl"))
 
 ########################################
 # Construction du problème type
-z = Variable("z", Complex)
+z1 = Variable("z1", Complex)
+z2 = Variable("z2", Complex)
 problemraw = Problem()
-add_variable!(problemraw, z)
-set_objective!(problemraw, imag(z))
-add_constraint!(problemraw, "ineq", abs2(z) << 4)
-add_constraint!(problemraw, "ineq_rot", real(z*exp(-im*π/4)) >> 0)
+add_variable!(problemraw, z1); add_variable!(problemraw, z2)
+set_objective!(problemraw, 3-abs2(z1)-0.5im*z1*conj(z2)^2+0.5im*z2^2*conj(z1))
+add_constraint!(problemraw, "eq1", (abs2(z1) - 0.25*z1^2 - 0.25*conj(z1)^2) == 1)
+add_constraint!(problemraw, "eq2", (abs2(z1) + abs2(z2)) == 3)
+add_constraint!(problemraw, "eq3", (im*z2 - im*conj(z2)) == 0)
+add_constraint!(problemraw, "ineq", (z2 + conj(z2)) >> 0)
 
 print(problemraw)
 
@@ -25,7 +28,7 @@ print(problem)
 # ▶ constraints:
 #    ineq_hi: 0 < 4 + (-1.0)*conj(z) * z
 # moment_cstr: 0 < 1.0
-relax_ctx = set_relaxation(problem, issparse = false, ismultiordered = false, d = 3)
+relax_ctx = set_relaxation(problem, issparse = false, ismultiordered = false, d = 2)
 
 ########################################
 # Construction du sparsity pattern
@@ -42,7 +45,7 @@ B_i = compute_Bibycstr(problem, momentmatrices, max_cliques, cliquevarsbycstr, o
 SDP_SOS = build_SDP_SOS(problem, max_cliques, B_i, cliquevarsbycstr, orderbyclique, relax_ctx);
 
 
-m, Zi, yα_re, yα_im = make_JuMPproblem(SDP_SOS, SCSSolver(maxiters=10000, eps=1e-10, verbose=false))
+m, Zi, yα_re, yα_im = make_JuMPproblem(SDP_SOS, SCSSolver(eps=1e-8, verbose=false))
 
 println("-----> SDP_SOS problem size: ", Base.summarysize(m)/1024, " ko")
 println("-----> JuMP problem size: ", Base.summarysize(m)/1024, " ko")
@@ -59,4 +62,4 @@ end
 
 println("\n\n----->Lagrange multipliers : yα =")
 yα = - getdual(yα_re) - im*getdual(yα_im)
-print_cmat(yα)
+print_cmat(transpose(yα))
