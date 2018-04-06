63b6ad3e693a10901f575f34c1088ca8a466dee4
Julie Sliwak <julie.sliwak@rte-france.com>
2018-04-06 14:30:46 +0200

    feastol = 1e-6 mip_int_var_strat = 2

--- MyJulia1.jl
+++ MyJulia1.jl
@@ -16,12 +16,12 @@ function MyJulia1(rawFile, genFile, contFile)
                           KTR_PARAM_SCALE=0,
                           KTR_PARAM_FEASTOL=1.0,
                           KTR_PARAM_OPTTOL=1.0,
-                          KTR_PARAM_FEASTOLABS=1e-8,
+                          KTR_PARAM_FEASTOLABS=1e-6,
                           KTR_PARAM_OPTTOLABS=1e-3,
                           KTR_PARAM_BAR_INITPT=2,
                           KTR_PARAM_PRESOLVE=0,
                           KTR_PARAM_HONORBNDS=0,
-                          KTR_PARAM_MIP_INTVAR_STRATEGY=0)
+                          KTR_PARAM_MIP_INTVAR_STRATEGY=2)
   tic()
   my_timer = @elapsed m, variables_jump = get_JuMP_cartesian_model(pb_global_real, mysolver)
   @printf("%-35s%10.6f s\n", "get_JuMP_cartesian_model", my_timer)
