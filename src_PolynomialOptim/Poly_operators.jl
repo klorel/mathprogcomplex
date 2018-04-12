410a5c36c8c066d62b53457cd5ffb4f276ff9ad9
GillesBareilles <gilles.bareilles@gmail.com>
2018-04-12 09:15:54 +0200

    conj(expo) fix, minor changes

--- src_PolynomialOptim/Poly_operators.jl
+++ src_PolynomialOptim/Poly_operators.jl
@@ -125,7 +125,11 @@ end
 function conj(expo::Exponent)
     expodict = Dict{Variable, Degree}()
     for (var, deg) in expo.expo
-        expodict[var] = conj(deg)
+        if iscomplex(var)
+            expodict[var] = conj(deg)
+        else
+            expodict[var] = deg
+        end
     end
     return Exponent(expodict)
 end
@@ -158,13 +162,13 @@ end
 
 
 ## Evaluate
+# NOTE: Point are sparse vectors, hence non referenced variables are assumed to be null.
 function evaluate(p::Polynomial, pt::Point)
     res=0
     expldeg = conjdeg = 0
     for (expo, λ) in p
         res += λ*evaluate(expo, pt)
     end
-    typeof(res)<:Polynomial && update_degree!(res)
     return res
 end
 
@@ -172,9 +176,8 @@ function evaluate(expo::Exponent, pt::Point)
     res=1
     for (var, deg) in expo.expo
         if haskey(pt, var)
-            res *= (evaluate(var, pt)^deg.explvar) * (conj(evaluate(var, pt))^deg.conjvar)
-        else
-            res *= var^deg.explvar * conj(var)^deg.conjvar
+            val = evaluate(var, pt)
+            res *= (val^deg.explvar) * (conj(val)^deg.conjvar)
         end
     end
     return res
