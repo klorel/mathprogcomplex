88eac949e1485011334bc8e8cb8149978a1c8fc4
Julie Sliwak <julie.sliwak@rte-france.com>
2018-05-02 09:19:21 +0200

    solutions Modified IEEE14

--- dev/create_comparison_csv.jl
+++ dev/create_comparison_csv.jl
@@ -6,14 +6,16 @@ function create_csv_scaling(folder)
     csv3 = readdlm(joinpath("..","knitro_runs","scaling2_nb_iter_$(folder).csv"), ';')
     csv4 = readdlm(joinpath("..","knitro_runs","scaling3_nb_iter_$(folder).csv"), ';')
 
-    f = open(joinpath("..", "knitro_runs","comparaisons_scaling_$folder"),"w")
-    write(f, "Step 1 scaling 0;Step 2 scaling 0;Step 3 scaling 0;Step 1 scaling 1;Step 2 scaling 1;Step 3 scaling 1;Step 1 scaling 2;Step 2 scaling 2;Step 3 scaling 2;Step 1 scaling 3;Step 2 scaling 3;Step 3 scaling 3; Step 1 min; Step 2 min;Step 3 min ")
+    f = open(joinpath("..", "knitro_runs","comparaisons_scaling_$folder.csv"),"w")
+    write(f, "Step 1 scaling 0;Step 2 scaling 0;Step 3 scaling 0;Step 1 scaling 1;Step 2 scaling 1;Step 3 scaling 1;Step 1 scaling 2;Step 2 scaling 2;Step 3 scaling 2;Step 1 scaling 3;Step 2 scaling 3;Step 3 scaling 3; Step 1 best; Step 2 best;Step 3 best\n ")
 
-    for i in 1:size(csv1,1)
+    for i in 2:size(csv1,1)
         min1 = min(csv1[i,1], csv2[i,1], csv3[i,1], csv4[i,1])
         min2 = min(csv1[i,2], csv2[i,2], csv3[i,2], csv4[i,2])
         min3 = min(csv1[i,3], csv2[i,3], csv3[i,3], csv4[i,3])
         write(f, "$(csv1[i,1]);$(csv1[i,2]);$(csv1[i,3]);$(csv2[i,1]);$(csv2[i,2]);$(csv2[i,3]);$(csv3[i,1]);$(csv3[i,2]);$(csv3[i,3]);$(csv4[i,1]);$(csv4[i,2]);$(csv4[i,3]);$min1;$min2;$min3 \n")
     end
 
-end    
+end
+
+create_csv_scaling("Phase_0_IEEE14")
