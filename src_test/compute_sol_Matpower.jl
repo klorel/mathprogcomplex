3c3f01a91914d26a1b6632532829f188eff94e9c
Julie Sliwak <julie.sliwak@rte-france.com>
2018-05-18 09:17:34 +0200

    --

--- src_test/compute_sol_Matpower.jl
+++ src_test/compute_sol_Matpower.jl
@@ -16,13 +16,23 @@ function build_and_solve_matpower_instance(instance_path)
     my_timer = @elapsed export_to_dat(pb_global_real, amplexportpath, init_point_real)
     @printf("%-35s%10.6f s\n", "export_to_dat", my_timer)
     _, t_knitro, _ = @timed run_knitro(amplexportpath, joinpath(pwd(),"..","src_ampl"))
+    sol = open(joinpath("..", "..", "data", "data_Matpower","solutions","sol.txt"), "r")
+    f = open(joinpath("..", "..", "data", "data_Matpower","solutions","$(instance_name[1:end-2])_sol.txt"), "w")
+    for l in readlines(sol)
+        write(f, l)
+        write(f, "\n")
+    end
+    close(sol)
+    close(f)
+
 end
 
 
 data_path = joinpath("..","..","data","data_Matpower","matpower")
 instances = sort(readdir(data_path))
 # instances = ["LMBM3.m", "WB2.m", "WB3.m", "WB5.m", "case118.m", "case1354pegase.m", "case13659pegase.m", "case14.m", "case145.m", "case1888rte.m", "case1951rte.m", "case2383wp.m", "case24_ieee_rts.m", "case2736sp.m", "case2737sop.m", "case2746wop.m", "case2746wp.m", "case2848rte.m", "case2868rte.m", "case2869pegase.m", "case30.m", "case300.m", "case300mod.m", "case3012wp.m", "case3120sp.m", "case3375wp.m", "case33bw.m", "case39.m", "case57.m", "case6468rte.m", "case6470rte.m","case6495rte.m","case6515rte.m", "case6ww.m", "case89pegase.m", "case9.m", "case9241pegase.m", "case9target.m", "case_ieee30.m", "case_illinois200.m"]
-# instances = [joinpath(data_path,"case9.m"),joinpath(data_path,"case14.m"), joinpath(data_path,"case30.m")]
+instances = ["LMBM3.m", "WB2.m", "WB3.m", "WB5.m", "case118.m", "case1354pegase.m", "case14.m", "case145.m", "case30.m", "case300.m", "case300mod.m",  "case39.m", "case57.m", "case6ww.m", "case89pegase.m", "case9.m", "case9target.m", "case_ieee30.m", "case_illinois200.m"]
+# instances = ["case9.m"#=,"case14.m", "case30.m"=#]
 println(instances)
 nb_instances = length(instances)
 
