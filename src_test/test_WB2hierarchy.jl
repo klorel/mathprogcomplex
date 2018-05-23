bf2334ac7a3649c3ef55c248f8ff3d72581f4d51
Gilles Bareilles <gilles.bareilles@gmail.com>
2018-05-23 14:49:03 +0200

    improve test WB2

--- src_test/test_WB2hierarchy.jl
+++ src_test/test_WB2hierarchy.jl
@@ -1,8 +1,8 @@
 ROOT = pwd()
 include(joinpath(ROOT, "src_SOShierarchy", "SOShierarchy.jl"))
 
-function print_primobj(primobjectives, sols)
-    @printf("%15s  %6s  %15s  %15s  | %15s  %20s  %15s\n", "q5min", "d", "objective", "CJ obj", "elapsed time", "tot. bytes alloc (MB)", "gctime")
+function print_primobj(io::IO, primobjectives, sols)
+    @printf(io, "%15s  %6s  %15s  %15s  %15s  | %15s  %20s  %15s\n", "v2max", "d", "objective", "CJ obj", "Δobjs", "elapsed time", "tot. bytes alloc (MB)", "gctime")
     for ((v2max, d), (cur_obj, t, bytes, gctime)) in primobjectives
         target_obj = -1
         if d == 2
@@ -11,15 +11,19 @@ function print_primobj(primobjectives, sols)
             target_obj = sols[v2max][2] # Optimal value
         end
 
+        CJ_obj = target_obj!=-1?target_obj:NaN
+        Δobj = target_obj!=-1?abs(cur_obj-target_obj):NaN
+        @printf(io, "%15f  %6i  %15f  %15f  %15f  | %15f  %20f  %15f\n", v2max, d/2, cur_obj, CJ_obj, Δobj, t, bytes, gctime)
         if target_obj!=-1 && !isapprox(cur_obj, target_obj, atol=1e-2)
             warn("Test WB2: (v2max, d) = ($v2max, $d) found objective is $cur_obj, expected $target_obj.")
         end
-
-        @printf("%15f  %6i  %15f  %15f  | %15f  %20f  %15f\n", v2max, d, cur_obj, target_obj!=-1?abs(cur_obj-target_obj):NaN, t, bytes, gctime)
     end
 end
 
 function main()
+    testfolder = joinpath("Mosek_runs", "testWB2extensive")
+    ispath(testfolder) && rm(testfolder, recursive=true)
+
     sols = SortedDict(0.976 => (2, 905.76, 905.76),
                       0.983 => (2, 905.73, 903.12),
                       0.989 => (2, 905.73, 900.84),
@@ -35,14 +39,17 @@ function main()
     for (v2max, vals) in sols
         dmax = vals[1]
 
-        problem = buildPOP_WB2(v2max=v2max, rmineqs=true)
+        problem = buildPOP_WB2(v2max=v2max, rmeqs=true)
 
         for d=2:2:2*dmax  # Complex d definition, twice real one...
+            info("Working on WB2, no ineqs, v2max=$v2max, d=$d")
+            logpath = joinpath(testfolder, "WB2_v2max_$(v2max)_d_$(d)_noeq")
+            ispath(logpath) && rm(logpath, recursive=true); mkpath(logpath)
+            println("Saving file at $logpath")
+
             relax_ctx = set_relaxation(problem; hierarchykind=:Real,
                                                 d = d)
 
-            logpath = joinpath("Mosek_runs", "WB2_v2max_$(v2max)_d_$(d)_noeq")
-            ispath(logpath) && rm(logpath, recursive=true); mkpath(logpath)
             (primobj, dualobj), t, bytes, gctime, memallocs = @timed run_hierarchy(problem, relax_ctx, logpath);
             primobjectives_noeqs[(v2max, d)] = (primobj, t, bytes / 10^6, gctime)
         end
@@ -52,24 +59,39 @@ function main()
     for (v2max, vals) in sols
         dmax = vals[1]
 
-        problem = buildPOP_WB2(v2max=v2max, rmineqs=false)
+        problem = buildPOP_WB2(v2max=v2max, rmeqs=false)
 
         for d=2:2:2*dmax  # Complex d definition, twice real one...
+            info("Working on WB2, ineqs, v2max=$v2max, d=$d")
+            logpath = joinpath(testfolder, "WB2_v2max_$(v2max)_d_$(d)_eq")
+            ispath(logpath) && rm(logpath, recursive=true); mkpath(logpath)
+            println("Saving file at $logpath")
+
             relax_ctx = set_relaxation(problem; hierarchykind=:Real,
                                                 d = d)
 
-            logpath = joinpath("Mosek_runs", "WB2_v2max_$(v2max)_d_$(d)_eq")
-            ispath(logpath) && rm(logpath, recursive=true); mkpath(logpath)
             (primobj, dualobj), t, bytes, gctime, memallocs = @timed run_hierarchy(problem, relax_ctx, logpath);
             primobjectives_eqs[(v2max, d)] = (primobj, t, bytes / 10^6, gctime)
         end
     end
 
     println("- No equality constraints:")
-    print_primobj(primobjectives_noeqs, sols)
+    print_primobj(STDOUT, primobjectives_noeqs, sols)
 
     println("- Equality constraints:")
-    print_primobj(primobjectives_eqs, sols)
+    print_primobj(STDOUT, primobjectives_eqs, sols)
+
+    logfile = joinpath(testfolder, "test_WB2.txt")
+    isfile(logfile) && rm(logfile)
+
+    info("Writing results to $logfile")
+    open(logfile, "w") do fout
+        println(fout, "- No equality constraints:")
+        print_primobj(fout, primobjectives_noeqs, sols)
+
+        println(fout, "- Equality constraints:")
+        print_primobj(fout, primobjectives_eqs, sols)
+    end
 
     return primobjectives_noeqs, primobjectives_eqs
 end
