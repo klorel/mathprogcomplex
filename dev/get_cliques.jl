7fe17048a0784860f5050b174e79696091a9a086
Julie Sliwak <julie.sliwak@rte-france.com>
2018-06-01 17:13:55 +0200

    --

--- dev/get_cliques.jl
+++ dev/get_cliques.jl
@@ -1,9 +1,44 @@
 ###
 
-# include(joinpath(pwd(),"..","src_PowSysMod", "PowSysMod_body.jl"))
+include(joinpath(pwd(),"src_PowSysMod", "PowSysMod_body.jl"))
 
 
+function convert_mipb_to_pb!(pb::Problem)
+    variables = pb.variables
+    for var in variables
+        println(var)
+        if var[2] == Bool
+            variable = Variable(var[1], Real)
+            add_constraint!(pb, "Bin_$(var[1])", variable^2 - variable == 0)
+            var[2] = Real
+        end
+    end
+    return pb
+end
 
+V1 = Variable("VOLT_1",Complex)
+V2 = Variable("VOLT_2",Complex)
+b = Variable("b", Bool)
+p_obj = V1*conj(V2)+2*abs2(V1)+1
+p_ctr1 = abs2(V1)
+p_ctr2 = V1*conj(V2)+abs2(V1)
+
+problem_poly=Problem()
+add_variable!(problem_poly,V1)
+add_variable!(problem_poly,V2)
+add_variable!(problem_poly,b)
+
+add_constraint!(problem_poly,"ctr1",0.95^2<< p_ctr1 <<1.05^2)
+add_constraint!(problem_poly,"ctr2",p_ctr2==0)
+add_constraint!(problem_poly,"testb", (b + V2 )==0)
+
+set_objective!(problem_poly,p_obj)
+
+print(problem_poly)
+
+PB_poly_real = pb_cplx2real(problem_poly)
+
+print(convert_mipb_to_pb!(PB_poly_real))
 
 
 function get_cliques(pb_real::Problem)
