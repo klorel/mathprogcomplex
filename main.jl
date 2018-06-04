b029f5b6bfe0b40b6115322cdc68a1e4f910a87d
Julie Sliwak <julie.sliwak@rte-france.com>
2018-06-04 14:54:43 +0200

    --

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 ROOT = pwd()
 include(joinpath(ROOT, "src_SOShierarchy", "SOShierarchy.jl"))
 include(joinpath(ROOT, "dev", "get_cliques.jl"))
+include(joinpath(ROOT, "src_Matpower", "get_cliques_matpower.jl"))
 
 
 function main()
@@ -14,21 +15,35 @@ function main()
     # relax_ctx = set_relaxation(problem; hierarchykind=:Real,
     #                                     # symmetries=[PhaseInvariance],
     #                                     d = 1)
+    #
+    # ###GOC
+    # data_path = joinpath("..", "data", "data_GOC")
+    # folder = "Phase_0_IEEE14_1Scenario"
+    # scenario = "scenario_1"
+    # folder_path = joinpath(data_path, folder)
+    # instance_path = joinpath(folder_path, scenario)
+    # raw = "powersystem.raw"
+    # gen = "generator.csv"
+    # con = "contingency.csv"
+    # rawfile = joinpath(instance_path,raw)
+    # genfile = joinpath(instance_path, gen)
+    # contfile = joinpath(instance_path, con)
+    # OPFpbs = load_OPFproblems(rawfile, genfile, contfile)
+    # introduce_Sgenvariables!(OPFpbs)
+    # ## Bulding optimization problem
+    # pb_global = build_globalpb!(OPFpbs)
+    # pb_global_real = pb_cplx2real(pb_global)
+    # problem = pb_global_real
+    # # problem = convert_mipb_to_pb(pb_global_real)
+    # relax_ctx = set_relaxation(problem; hierarchykind=:Real,
+    #                                     # symmetries=[PhaseInvariance],
+    #                                     issparse=true,
+    #                                     d = 2)
 
-    ###GOC
-    data_path = joinpath("..", "data", "data_GOC")
-    folder = "Phase_0_IEEE14_1Scenario"
-    scenario = "scenario_1"
-    folder_path = joinpath(data_path, folder)
-    instance_path = joinpath(folder_path, scenario)
-    raw = "powersystem.raw"
-    gen = "generator.csv"
-    con = "contingency.csv"
-    rawfile = joinpath(instance_path,raw)
-    genfile = joinpath(instance_path, gen)
-    contfile = joinpath(instance_path, con)
-    OPFpbs = load_OPFproblems(rawfile, genfile, contfile)
-    introduce_Sgenvariables!(OPFpbs)
+    ##Matpower
+    instance = "case30.m"
+    instance_path = joinpath(pwd(),"..","data", "data_Matpower", "matpower", instance)
+    OPFpbs = load_OPFproblems(MatpowerInput, instance_path)
     ## Bulding optimization problem
     pb_global = build_globalpb!(OPFpbs)
     pb_global_real = pb_cplx2real(pb_global)
@@ -36,8 +51,8 @@ function main()
     # problem = convert_mipb_to_pb(pb_global_real)
     relax_ctx = set_relaxation(problem; hierarchykind=:Real,
                                         # symmetries=[PhaseInvariance],
-                                        issparse=true,
-                                        d = 2)
+                                        # issparse=true,
+                                        d = 1)
 
 
 
@@ -49,8 +64,10 @@ function main()
 
     ########################################
     # Construction du sparsity pattern, extension chordale, cliques maximales.
-    # max_cliques = get_maxcliques(relax_ctx, problem)
-    max_cliques = get_cliques(problem)
+    max_cliques = get_maxcliques(relax_ctx, problem)
+    # max_cliques = get_cliques(problem)
+    # max_cliques = get_cliques_matpower(instance_path)
+
 
     println("\n--------------------------------------------------------")
     println("max cliques =")
