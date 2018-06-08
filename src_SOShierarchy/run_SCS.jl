48d4f23d08d3a6498604f160c63f8e1f34e3fa2e
Gilles Bareilles <gilles.bareilles@gmail.com>
2018-06-08 14:28:48 +0200

    [WIP]

--- src_SOShierarchy/run_SCS.jl
+++ src_SOShierarchy/run_SCS.jl
@@ -3,87 +3,89 @@ include(joinpath(ROOT, "src_SOShierarchy", "SOShierarchy.jl"))
 
 using Convex, SCS
 
-function build_Convex_SDPPrimal(primalSDP::MomentRelaxation{Float64}, mysolver)
-
-    m = Model(solver = mysolver)
-
-    # Collect all moments
-    moments = Set{Moment}(keys(primalSDP.objective))
-    for (key, mmtmat) in primalSDP.constraints
-        for ((γ, δ), p) in mmtmat.mm
-            union!(moments, keys(p))
-        end
+function set_variables!(matrix_variables, scalar_variables, primalSDP)
+    for (names, block) in primalSDP.name_to_sdpblock
+        n = length(block.var_to_id)
+        matrix_variables[names] = Convex.Variable(n, n)
     end
 
-    # Build LMI index mappings
-    ctr_2_keymap = Dict()
-    for (key, mmtmat) in primalSDP.constraints
-        expokeys = Set{Exponent}()
-        for (γ, δ) in keys(mmtmat.mm)
-            union!(expokeys, [γ, δ])
-        end
-
-        expo_to_ind = Dict{Exponent, Int}()
-        i=0
-        for expo in sort(collect(expokeys))
-            i+=1
-            expo_to_ind[expo] = i
-        end
-
-        ctr_2_keymap[key] = expo_to_ind
-
-        info("- ctr is: ", key)
-        for expo in sort(collect(keys(expo_to_ind)))
-            ind = expo_to_ind[expo]
-            println("    $expo      $ind")
-        end
+    for varname in keys(primalSDP.scalvar_to_id)
+        scalar_variables[varname] = Convex.Variable()
     end
-
-    #Define JuMP variables
-    moment_to_JuMPvar = Dict{Moment, JuMP.Variable}()
-    for moment in sort(collect(moments))
-        moment_to_JuMPvar[moment] = @variable(m, basename=format_string(moment.conj_part)*format_string(moment.expl_part)*moment.clique)
-    end
-
-    # Define objective
-    objective = 0
-    for (mmt, val) in primalSDP.objective
-        objective += moment_to_JuMPvar[mmt]*val
-    end
-    @objective(m, Min, objective)
-
-
-    # Define constraints
-    warn("\nBuilding constraints")
-    for ((ctr, clique), mmtmat) in primalSDP.constraints
-        println("   -> $ctr, $clique")
-
-        moment_to_scalmat = Dict{Moment, Any}()
-
-        for ((γ, δ), p) in mmtmat.mm
-            for (moment, val) in p
-                !haskey(moment_to_scalmat, moment) && moment_to_scalmat[moment] = ([],[],[])
-                moment_to_scalmat[moment][1] = ctr_2_keymap[(ctr, clique)][γ]
-                moment_to_scalmat[moment][2] = ctr_2_keymap[(ctr, clique)][δ]
-                moment_to_scalmat[moment][3] = val
-            end
-        end
-
-        LMIbody = 0
-        for (moment, triplets) in moment_to_scalmat
-            LMIbody += moment_to_JuMPvar[moment] * sparse(triplets...)
-        end
-        @constraint(m, SDP, LMIbody)
-    end
-
-    return m
 end
 
-sparse([1, 2, 3], [1, 2, 3], [0, 2, 0])
+function build_Convex_SDPPrimal(primalSDP::SDP_Problem{Float64}, mysolver)
+
+    matrix_variables = Dict{String, Any}()
+    scalar_variables = Dict{String, Any}()
+
+    set_variables!(matrix_variables, scalar_variables, primalSDP)
+
+    objective = 0
+    constraints = Dict{SDP_Moment, Any}()
+    for ((sdpmoment, matname, γ, δ), λ) in primalSDP.matrices
+        i = primalSDP.name_to_sdpblock[matname].var_to_id[γ]
+        j = primalSDP.name_to_sdpblock[matname].var_to_id[δ]
+        n = size(matrix_variables[matname], 1)
+
+        if sdpmoment in primalSDP.obj_keys
+
+            objective += vecdot(matrix_variables[matname], sparse([i], [j], [λ], n, n))
+
+            # objective += matrix_variables[matname][primalSDP.name_to_sdpblock[matname].var_to_id[γ],
+            #                                        primalSDP.name_to_sdpblock[matname].var_to_id[δ]] * λ
+            println(" Adding to objective, Zi $matname, i,j = $i, $j  *\t $λ")
+        else
+            !haskey(constraints, sdpmoment) && (constraints[sdpmoment] = 0)
+            # constraints[sdpmoment] += matrix_variables[matname] * sparse([primalSDP.name_to_sdpblock[matname].var_to_id[γ]],
+            #                                                             [primalSDP.name_to_sdpblock[matname].var_to_id[δ]],
+            #                                                             [λ],
+            #                                                             size(matrix_variables[matname])...)
+            # constraints[sdpmoment] += matrix_variables[matname][primalSDP.name_to_sdpblock[matname].var_to_id[γ],
+            #                                                     primalSDP.name_to_sdpblock[matname].var_to_id[δ]] * λ
+
+            constraints[sdpmoment] += vecdot(matrix_variables[matname], sparse([i], [j], [λ], n, n))
+
+        end
+    end
+
+    for ((sdpmoment, scalvar), λ) in primalSDP.linear
+        if sdpmoment in primalSDP.obj_keys
+            objective += scalar_variables[scalvar] * λ
+            println("Ading to objective, $scalvar \t $λ")
+        else
+            !haskey(constraints, sdpmoment) && (constraints[sdpmoment] = 0)
+            constraints[sdpmoment] += scalar_variables[scalvar] * λ
+        end
+    end
+
+    for (sdpmoment, λ) in primalSDP.cst_ctr
+        if sdpmoment in primalSDP.obj_keys && λ!=0
+            objective += λ
+            println("Adding to objective $λ")
+        elseif λ != 0.0
+            !haskey(constraints, sdpmoment) && (constraints[sdpmoment] = 0)
+            constraints[sdpmoment] += λ
+        end
+    end
+
+    ctrs = Convex.Constraint[]
+
+    for ctrname in sort(collect(keys(constraints)))
+        push!(ctrs, (constraints[ctrname] == 0))
+    end
+    for (matname, var) in matrix_variables
+        push!(ctrs, var in :SDP)
+    end
+
+    problem = minimize(objective, ctrs)
+    return problem
+end
+
 
 
 function main()
-    problem = buildPOP_WB2(v2max=1.022, setnetworkphase=true)
+    problem = buildPOP_WB2(v2max=1.022, setnetworkphase=false, rmeqs=true)
     relax_ctx = set_relaxation(problem; hierarchykind=:Real,
                                         # symmetries=[PhaseInvariance],
                                         d = 1)
@@ -116,9 +118,36 @@ function main()
     println("\n--------------------------------------------------------")
     println("mmtrel_pb = $mmtrel_pb")
 
-    println("\n\n\n")
-    m = build_JuMP_SDPDual(mmtrel_pb, SCSSolver())
-    show(m)
+    # Convert to a primal SDP problem
+    sdpinstance = build_SDPInstance(relax_ctx, mmtrel_pb)
+
+    logpath=joinpath("Mosek_runs", "SCSdev")
+    ispath(logpath) && rm(logpath, recursive=true)
+    mkpath(logpath)
+    export_SDP(sdpinstance, logpath, renamemoments=false)
+
+    main2()
 end
 
-main()
+function main2()
+    logpath=joinpath("Mosek_runs", "SCSdev")
+    sdp = build_mosekpb(logpath)
+
+    println("\n\n\n")
+    m = build_Convex_SDPPrimal(sdp, MosekSolver())
+
+    solve!(m)
+    return m.status
+end
+
+
+# main()
+# main2()
+
+function main3()
+    problem = buildPOP_WB2(rmeqs=false) # v2max=0.976
+
+    logpath = joinpath("Mosek_runs", "totoa")
+    mkpath(logpath)
+    export_to_dat(problem, joinpath("Mosek_runs", "totoa"))
+end
